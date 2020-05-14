825f57fedcea8b27a94e167a0949b5051cfd1349
gabrielhwagner <gabrielwagner789@gmail.com>
2020-05-14 01:45:26 -0300

    [04] Cria função de cálculo romano para arábico

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -100,11 +100,31 @@ export class AppComponent implements OnInit{
     }
   }
 
+  converterRomanosParaArabicos(valor) {
+    let creditos = 0;
+
+    for(let i=0,j=1; j<=valor.length; i++, j++) {
+      const valorI = this.descobreValorRomano(valor[i]);
+      let valorJ = 0;
+      if(valor[j]) {
+        valorJ = this.descobreValorRomano(valor[j]);
+      }
+      if(valorI >= valorJ) {
+        creditos += valorI;
+      } else if (valorI < valorJ) {
+        creditos += valorJ - valorI;
+        i++;j++;
+      }
+    }
+
+    this.convertido += `vale ${creditos}`; 
+  }
+
   descobreValorRomano(valorRomano) {
     const item = this.romanos.find((romano) => romano.key === valorRomano);
     return item.value;
   }
-  
+
   validaInsercaoMoeda(frase) {
     const index = this.buscaPosicaoPalavra(frase, 'vale');
     const chave = frase[index - 1];
